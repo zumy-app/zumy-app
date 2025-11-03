ccbfbaae0fd3386d74d3016e8972cd7446e3ee0d
PB <info@zumy.app>
2025-11-02 20:06:31 -0600

    Setup deployment to Cloudflare Pages with wrangler

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/zumy-app/',
+  base: '/',
   server: {
     host: "::",
     port: 8080,
